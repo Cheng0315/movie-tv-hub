79c5f324755252142973caa26baad3a178f37144
Cheng Vue <csalvue@gmail.com>
2019-06-24 15:17:03 -0700

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navBar'
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <NavBar/>
     </div>
   );
 }
