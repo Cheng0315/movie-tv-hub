65e0e3186ceb5bb74d6464b86dd4f47165c2e324
Cheng Vue <csalvue@gmail.com>
2019-07-09 15:14:53 -0700

    add movieTvComponent for viwing info about a movie or tv

---
+++ src/components/movieTvDetails.js
@@ -0,0 +1,27 @@
+import React from 'react'
+ 
+class MovieTvDetails extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src="https://amc-theatres-res.cloudinary.com/v1553695740/amc-cdn/production/2/movies/45800/45840/PosterDynamic/75045.jpg" />
+        </div>
+        <div>
+          Details
+        </div>
+        <div>
+          Cast
+        </div>
+        <div>
+          Trailers
+        </div>
+        <div>
+          Reviews
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MovieTvDetails;
