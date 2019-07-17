4bed405b3e0a74e6a12c9a7f776dad5ab1313187
Cheng Vue <csalvue@gmail.com>
2019-07-16 19:11:46 -0700

    add store.js component

--- src/components/movieTvDetails.js
+++ src/components/movieTvDetails.js
@@ -6,12 +6,13 @@ class MovieTvDetails extends React.Component {
     return (
       <div>
         <div>
-          <img src="https://amc-theatres-res.cloudinary.com/v1553695740/amc-cdn/production/2/movies/45800/45840/PosterDynamic/75045.jpg" />
+          <h3>One Punch Man</h3>
+          <img src="https://amc-theatres-res.cloudinary.com/v1553695740/amc-         cdn/production/2/movies/45800/45840/PosterDynamic/75045.jpg" />
         </div>
         <div>
           Details
           <p>
-            Summary about the movie/tv show
+          Saitama is a hero who only became a hero for fun. After three years of “special” training, though, he’s become so   strong that he’s practically invincible. In fact, he’s too strong—even his mightiest opponents are taken out with a single punch, and it turns out that being devastatingly powerful is actually kind of a bore. With his passion for being a hero lost along with his hair, yet still faced with new enemies every day, how much longer can he keep it going?
           </p>
         </div>
         <div>
@@ -24,9 +25,11 @@ class MovieTvDetails extends React.Component {
         </div>
         <div>
           Reviews
-          <p>
-            list of reviews about the movie/tv show
-          </p>
+         <ul>
+	        <li>
+					 list of reviews about the movie or tv shows
+		      </li>
+				 </ul>
         </div>
       </div>
     )
