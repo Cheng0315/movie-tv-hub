7c5296d71f0d8f4d7e2011ead992b4a8c3d5109a
Cheng Vue <csalvue@gmail.com>
2019-07-23 23:31:55 -0700

    add top rated movies, upcoming movies and now playing movies to initial state in movieTvReducer file

--- src/reducers.js/movieTvReducer.js
+++ src/reducers.js/movieTvReducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-  popularMovies: []
+  popularMovies: [],
+  nowPlayingMovies: [],
+  topRatedMovies: [],
+  upcomingMovies: []
 }
 
 export default function plateReducer(state = initialState, action) {
   switch(action.type){
     case 'START_ADDING_MOVIES':
-      return {...state, loadingPlates: true}
+      return {...state, loadingMovies: true}
     default: 
       return state
   }
