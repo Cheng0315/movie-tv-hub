4081cac1969479b2ddff26d3a8bd888f97fdc0ff
Cheng Vue <csalvue@gmail.com>
2019-07-30 22:24:42 -0700

    add fetch movies cases to movieTvReducer

--- src/reducers.js/movieTvReducer.js
+++ src/reducers.js/movieTvReducer.js
@@ -1,4 +1,5 @@
 const initialState = {
+  loadingMovies: false,
   popularMovies: [],
   nowPlayingMovies: [],
   topRatedMovies: [],
@@ -9,6 +10,14 @@ export default function plateReducer(state = initialState, action) {
   switch(action.type){
     case 'START_ADDING_MOVIES':
       return {...state, loadingMovies: true}
+    case 'FETCH_NOW_PLAYING_MOVIES':
+      return {...state, loadingMovies: false, nowPlayingMovies: action.payload}
+    case 'FETCH_POPULAR_MOVIES':
+      return {...state, loadingMovies: false, popularMovies: action.payload}
+    case 'FETCH_TOP_RATED_MOVIES':
+      return {...state, loadingMovies: false, topRatedMovies: action.payload}
+    case 'FETCH_UPCOMING_MOVIES':
+      return {...state, loadingMovies: false, upcomingMovies: action.payload}
     default: 
       return state
   }
